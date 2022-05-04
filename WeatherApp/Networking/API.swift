c38c7f040f651687239d6a8faf425dbce38d521e
bascor1207 <bascor1207@gmail.com>
2022-05-04 11:26:16 +0200

    feature: WeatherApp

---
+++ WeatherApp/Networking/API.swift
@@ -0,0 +1,7 @@
+// API.swift
+
+import Foundation
+
+struct API {
+    static let key = "d245eb12eb9cff712efca89b181dc2af"
+}
---
+++ WeatherApp/Views/MenuHeaderView.swift
@@ -0,0 +1,43 @@
+// MenuHeaderView.swift
+
+import SwiftUI
+
+struct MenuHeaderView: View {
+    
+    @ObservedObject var cityVM: CityViewViewModel
+    
+    @State private var searchTerm = "Rechercher une ville"
+    
+    var body: some View {
+        HStack {
+            TextField("", text: $searchTerm)
+                .padding(.leading, 20)
+            
+            Button {
+                cityVM.city = searchTerm
+            }
+            label: {
+                ZStack {
+                    Image(systemName: "magnifyingglass")
+                }
+            }
+            .frame(width: 50, height: 50)
+        }
+        .foregroundColor(.white)
+        .padding()
+        .background(ZStack (alignment: .leading) {
+            Image(systemName: "magnifyingglass.circle")
+                .foregroundColor(.white)
+                .padding(.leading, 10)
+            
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.blue.opacity(0.5))
+        })
+    }
+}
+
+struct MenuHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
